6094abaf2bd401dcb5adb5f1a44041a2edd4b7a4
Victor Martin <victor.martin.garcia@gmail.com>
2019-09-30 15:32:50 +0200

    refactor AAA pattern

--- src/test/java/edu/udg/caes/CalculatorTest.java
+++ src/test/java/edu/udg/caes/CalculatorTest.java
@@ -6,47 +6,56 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
     @Test
     public void twoAndThreeIsFive(){
-        final long result = new Calculator().add(2, 3);
+        Calculator c = new Calculator();
+        final long result = c.add(2, 3);
         assertEquals(result, 5L);
     }
     @Test
-    public void twoAndZeroIsTwo() throws Exception {
-        final long result = new Calculator().add(2, 0);
+    public void twoAndZeroIsTwo(){
+        Calculator c = new Calculator();
+        final long result = c.add(2, 0);
         assertEquals(result, 2L);
     }
     @Test
-    public void twoAndMinusTwoIsZero() throws Exception {
-        final long result = new Calculator().add(2, -2);
+    public void twoAndMinusTwoIsZero(){
+        Calculator c = new Calculator();
+        final long result = c.add(2, -2);
         assertEquals(result, 0L);
     }
     @Test
-    public void threeMinusTwoIsOne() throws Exception {
-        final long result = new Calculator().subtract(3, 2);
+    public void threeMinusTwoIsOne(){
+        Calculator c = new Calculator();
+        final long result = c.subtract(3, 2);
         assertEquals(result, 1L);
     }
     @Test
-    public void threeMinusThreeIsZero() throws Exception {
-        final long result = new Calculator().subtract(3, 3);
+    public void threeMinusThreeIsZero(){
+        Calculator c = new Calculator();
+        final long result = c.subtract(3, 3);
         assertEquals(result, 0L);
     }
     @Test
-    public void threeMinusMinusThreeIsSix() throws Exception {
-        final long result = new Calculator().subtract(3, -3);
+    public void threeMinusMinusThreeIsSix(){
+        Calculator c = new Calculator();
+        final long result = c.subtract(3, -3);
         assertEquals(result, 6L);
     }
     @Test
-    public void threeXThreeIsNine() throws Exception {
-        final long result = new Calculator().multiply(3, 3);
+    public void threeXThreeIsNine(){
+        Calculator c = new Calculator();
+        final long result = c.multiply(3, 3);
         assertEquals(result, 9L);
     }
     @Test
-    public void threeXZeroIsZero() throws Exception {
-        final long result = new Calculator().multiply(3, 0);
+    public void threeXZeroIsZero(){
+        Calculator c = new Calculator();
+        final long result = c.multiply(3, 0);
         assertEquals(result, 0L);
     }
     @Test
-    public void threeXMinusThreeIsMinusNine() throws Exception {
-        final long result = new Calculator().multiply(3, -3);
+    public void threeXMinusThreeIsMinusNine(){
+        Calculator c = new Calculator();
+        final long result = c.multiply(3, -3);
         assertEquals(result, -9L);
     }
 }
